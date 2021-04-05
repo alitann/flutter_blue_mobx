3a2b78bee30f1a0c62e7183330a72b4a5b52949c
Ali Tanrıverdi <alitanriverdi@outlook.com>
2021-04-05 10:39:05 +0300

    Bluetooth state control added

--- lib/bluetooth/view/device_list.dart
+++ lib/bluetooth/view/device_list.dart
@@ -1,6 +1,7 @@
 import 'package:blue/bluetooth/view/device_detail.dart';
 import 'package:blue/bluetooth/view_model/bluetooth_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
 import '../device_type.dart';
@@ -30,46 +31,66 @@ class _DeviceListState extends State<DeviceList> {
               Column(
                 children: [
                   _buildFindButton(),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Observer(builder: (_) {
-                      return Text(
-                          '${bluetoothViewModel.devices.length.toString()} device(s) found',
-                          style: Theme.of(context).textTheme.headline6);
-                    }),
-                  ),
+                  buildBuildDevicesFoundText(context),
                 ],
               ),
-              Observer(builder: (_) {
-                return Flexible(
-                  child: ListView.separated(
-                      shrinkWrap: true,
-                      separatorBuilder: (context, index) => Divider(),
-                      itemCount: bluetoothViewModel.devices.length,
-                      itemBuilder: (BuildContext ctxt, int index) {
-                        var device = bluetoothViewModel.devices[index];
-                        return ListTile(
-                          title: Text(
-                            device.id.toString(),
-                          ),
-                          subtitle: Text(device.name),
-                          leading: Icon(Icons.bluetooth),
-                          trailing: Text(device.type.rawValue),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        DeviceDetail(selectedDevice: device)));
-                          },
-                        );
-                      }),
-                );
-              }),
+              buildDeviceListView(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  StreamBuilder<BluetoothState> buildDeviceListView() {
+    return StreamBuilder<BluetoothState>(
+        stream: bluetoothViewModel.flutterBlue.state,
+        builder: (context, snapshot) {
+          final bluetoothState = snapshot.data;
+          if (bluetoothState == BluetoothState.on) {
+            return Observer(builder: (_) {
+              return Flexible(
+                child: ListView.separated(
+                    shrinkWrap: true,
+                    separatorBuilder: (context, index) => Divider(),
+                    itemCount: bluetoothViewModel.devices.length,
+                    itemBuilder: (BuildContext ctxt, int index) {
+                      var device = bluetoothViewModel.devices[index];
+                      return ListTile(
+                        title: Text(
+                          device.id.toString(),
+                        ),
+                        subtitle: Text(device.name),
+                        leading: Icon(Icons.bluetooth),
+                        trailing: Text(device.type.rawValue),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      DeviceDetail(selectedDevice: device)));
+                        },
+                      );
+                    }),
+              );
+            });
+          } else
+            return Center(
+                child: Text('Your device\'s bluetooth is not available'));
+        });
+  }
+
+  Padding buildBuildDevicesFoundText(BuildContext context) =>
+      buildDevicesFoundText(context);
+
+  Padding buildDevicesFoundText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Observer(builder: (_) {
+        return Text(
+            '${bluetoothViewModel.devices.length.toString()} device(s) found',
+            style: Theme.of(context).textTheme.headline6);
+      }),
     );
   }
 
